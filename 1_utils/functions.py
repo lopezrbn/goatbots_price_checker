d55a910fcf6cd610ec15f2e3e73d25d7bd078de4
Rubén López <lopezrbn@gmail.com>
2024-07-12 08:52:47 +0200

    Corrected bug in delta of prices calculation

--- 1_utils/functions.py
+++ 1_utils/functions.py
@@ -67,15 +67,18 @@ def price_check(cardID=None, name=None, cardset=None, foil=None, range_time="3m"
     # Initialize the prices dictionary
     prices = {}
     # Define the range times
-    range_times_words = ["1d", "1w", "1m", "3m", "6m"]
-    range_times = [2, 7, 30, 90, 180]
+    range_times_words = ["today", "1d", "1w", "1m", "3m", "6m"]
+    range_times = [1, 2, 7, 30, 90, 180]
     # Get the prices for each range
     for range_time_word, range_time in zip(range_times_words, range_times):
         prices[range_time_word] = {}
         df_temp = df_prices_filtered.iloc[:, -range_time:]
         prices[range_time_word]["day"] = float(df_temp.iloc[:, -range_time].values[0])
+        print(f"Price today: {price_today}")
+        print(f"Price {range_time_word} day: {prices[range_time_word]['day']}")
         prices[range_time_word]["delta"] = ((price_today - prices[range_time_word]["day"]) / prices[range_time_word]["day"]) if prices[range_time_word]["day"] != 0 else 0.0
         prices[range_time_word]["delta"] = round(prices[range_time_word]["delta"], 2)
+        print(prices[range_time_word]["delta"])
         prices[range_time_word]["min"] = float(df_temp.min().min())
         prices[range_time_word]["max"] = float(df_temp.max().max())
         prices[range_time_word]["today_vs_range"] = (price_today - prices[range_time_word]["min"]) / (prices[range_time_word]["max"] - prices[range_time_word]["min"]) if prices[range_time_word]["max"] != prices[range_time_word]["min"] else 0.0
