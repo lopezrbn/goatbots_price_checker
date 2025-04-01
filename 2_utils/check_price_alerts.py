ff67ebaaa9ff894879469a8eb452ef82e4002c44
Rubén López <lopezrbn@gmail.com>
2025-04-01 15:44:28 +0200

    Added new section "Watchlist cards" in the daily email

--- 2_utils/check_price_alerts.py
+++ 2_utils/check_price_alerts.py
@@ -6,10 +6,7 @@ import pandas as pd
 import json
 
 
-MIN_PRICE_THRESHOLD = 0.5
-
-
-def _check_price_alert(cardID, range_time="1m", sell_alert=0.75, buy_alert=0.25, verbose=False):
+def _check_price_alert(cardID, range_time=config.RANGE_TIME, sell_alert=config.PRICE_PERC_SELL_ALERT, buy_alert=config.PRICE_PERC_SELL_ALERT, verbose=False):
     if verbose:
         print(f"\tChecking price alert for cardID {cardID}...")
     # today_vs_range, price_min_range, price_today, price_max_range = fun.price_check(cardID=cardID, range_time=range_time)
@@ -42,21 +39,27 @@ def check_price_alerts(range_time="1m", verbose=False):
     df = pd.read_parquet(config.PATH_DF_PRICES)
 
     # Load fav cards
-    with open(config.PATH_FAV_CARDS, "r") as f:
+    with open(config.PATH_COLLECTION_CARDS_JSON, "r") as f:
         fav_cards = json.load(f)
 
-    # Check alerts
-    alerts = []
-    
+    # Load watchlist cards
+    with open(config.PATH_WATCHLIST_CARDS, "r") as f:
+        watchlist_cards = json.load(f)
+
+    # Check alerts for fav cards and watchlist
+    alerts_fav = []
+    alerts_watchlist = []
     for cardID in fav_cards.keys():
-        alerts.append(_check_price_alert(cardID, range_time=range_time))
+        alerts_fav.append(_check_price_alert(cardID, range_time=range_time))
+    for cardID in watchlist_cards.keys():
+        alerts_watchlist.append(_check_price_alert(cardID, range_time=range_time))
 
     # Extract sell and buy alerts
     sell_alerts = {}
     buy_alerts = {}
-    for alert in alerts:
+    for alert in alerts_fav:
         prices = alert[3]
-        if prices["today"]["day"] >= MIN_PRICE_THRESHOLD:
+        if prices["today"]["day"] >= config.MIN_PRICE_THRESHOLD:
             if alert[0] == 1:
                 sell_alerts[alert[1]] = {
                     "name": fav_cards[alert[1]]["name"],
@@ -90,13 +93,35 @@ def check_price_alerts(range_time="1m", verbose=False):
                     "max_range": prices[range_time]["max"]
                 }
 
+    # Extract watchlist alerts
+    watchlist_alerts = {}
+    for alert in alerts_watchlist:
+        prices = alert[3]
+        watchlist_alerts[alert[1]] = {
+            "name": watchlist_cards[alert[1]]["name"],
+            "cardset": watchlist_cards[alert[1]]["cardset"],
+            "foil": watchlist_cards[alert[1]]["foil"],
+            "price": prices["today"]["day"],
+            "Δ1d": prices["1d"]["delta"],
+            "Δ1w": prices["1w"]["delta"],
+            "Δ1m": prices["1m"]["delta"],
+            "Δ3m": prices["3m"]["delta"],
+            "Δ6m": prices["6m"]["delta"],
+            "range_time": alert[2],
+            "%range": prices[range_time]["today_vs_range"],
+            "min_range": prices[range_time]["min"],
+            "max_range": prices[range_time]["max"]
+        }
+
     # Save alerts
     if verbose:
         print("\tSaving alerts...")
     with open(config.PATH_SELL_ALERTS, "w") as f:
-        json.dump(sell_alerts, f)
+        json.dump(sell_alerts, f, indent=4)
     with open(config.PATH_BUY_ALERTS, "w") as f:
-        json.dump(buy_alerts, f)
+        json.dump(buy_alerts, f, indent=4)
+    with open(config.PATH_WATCHLIST_ALERTS, "w") as f:
+        json.dump(watchlist_alerts, f, indent=4)
     if verbose:
         print("\t\tAlerts saved successfully.")
 
--- 2_utils/send_email.py
+++ 2_utils/send_email.py
@@ -22,20 +22,25 @@ def send_email():
         sell_alerts = pd.read_json(f, orient="index")
     with open(config.PATH_BUY_ALERTS) as f:
         buy_alerts = pd.read_json(f, orient="index")
+    with open(config.PATH_WATCHLIST_ALERTS) as f:
+        watchlist_alerts = pd.read_json(f, orient="index")
 
     # Sort alerts
     sell_alerts = sell_alerts.sort_values(by=["Δ1d", "name", "cardset", "foil"], ascending=[False, True, True, True])
     buy_alerts = buy_alerts.sort_values(by=["Δ1d", "name", "cardset", "foil"], ascending=[True, True, True, True])
+    watchlist_alerts = watchlist_alerts.sort_values(by=["name", "Δ1d", "cardset", "foil"], ascending=[True, True, True, True])
 
     # Set columns to percentage
     columns = ["Δ1d", "Δ1w", "Δ1m", "Δ3m", "Δ6m", "%range"]
     for col in columns:
         sell_alerts[col] = sell_alerts[col].map("{:.0%}".format)
         buy_alerts[col] = buy_alerts[col].map("{:.0%}".format)
+        watchlist_alerts[col] = watchlist_alerts[col].map("{:.0%}".format)
 
     # Transform dataframes to html
     sell_alerts_html = sell_alerts.to_html(index=False)
     buy_alerts_html = buy_alerts.to_html(index=False)
+    watchlist_alerts_html = watchlist_alerts.to_html(index=False)
 
     # Create message
     message = MIMEMultipart("alternative")
@@ -49,6 +54,8 @@ def send_email():
         <head></head>
         <body>
             <p>Here's the daily update on prices:</p>
+            <h2>Watchlist Alerts</h2>
+            {watchlist_alerts_html}
             <h2>Sell Alerts</h2>
             {sell_alerts_html}
             <h2>Buy Alerts</h2>
